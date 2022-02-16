170026cc51c9c65a172aabe664a9a5883f17948a
tanimon <8575113+tanimon@users.noreply.github.com>
2022-02-16 15:51:26 +0900

    Integrate LIFF into the React app

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import liff from '@line/liff';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const liffId = process.env.REACT_APP_LIFF_ID!;
+console.log(`liffId: ${liffId}`);
+
+liff.init({ liffId }).then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
